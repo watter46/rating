2d2396b8af26716f517679b8ee27c15aafeb35fd
watter46 <w.aoki.14@gmail.com>
2023-12-14 05:26:42 +0900

    add: #9_先発のメンバーを取得できるようにする

--- src/app/Http/Controllers/PlayerController.php
+++ src/app/Http/Controllers/PlayerController.php
@@ -2,124 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\EvaluatePlayerRequest;
-use App\Http\Resources\PlayerResource;
 use Exception;
-use Illuminate\Support\Str;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\File;
-use App\Http\Controllers\PositionType;
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\File;
+
+use App\UseCases\Player\GetLineupUseCase;
 
 
 final class PlayerController extends Controller
 {
-    public function index()
+    public function index(GetLineupUseCase $getLineup)
     {
         try {
-            $players = collect([
-                'df'  => [2, 3, 4, 5],
-                'mid' => [6, 7, 8, 9],
-                'fw'  => [10, 11],
-            ])->reverse();
+            $fixtureId = 1035327;
 
-            $path = app_path('Template/squads');
-
-            $fileName = $path.'/'.now()->year.'_post_summer.json';
-
-            $json = File::get($fileName);
-
-            $players = json_decode($json)->response[0]->players;
-
-            // $json = File::get(app_path('Template/players.json'));
-
-            // $players = json_decode($json)->players;
-
-            $statistics = $this->startingXI();
-
-            $filtered = collect($players)->whereIn('id', $statistics->pluck('player.id')->toArray());
-
-            // $filteredNotIn = (object) collect($statistics)->whereNotIn('name', collect($players)->pluck('name')->toArray());
-            
-            // statisticsには未登録の選手も取得される
-            // 20選手取得される
-
-            // dd($filtered);
-
-            // PlayerIdで統一する
-
-            $players = $filtered
-                ->map(function ($player) use ($statistics) {
-                    $path = public_path('images').'/'.now()->year.'_S_'.$player->id;
-
-                    $image = File::exists($path) ? base64_encode(File::get($path)) : '';
-
-                    // アルファベット + . の場合と普通の表記で場合分けする
-
-                    // dd(PositionType::from($player->player->position)->name);
-                    // $statistics->sole(fn ($statistic) => $statistic['id'] === $player->number);
-                    
-                    // return (object)[
-                    //     'id'       => $player->id,
-                    //     'name'     => $player->name,
-                    //     'number'   => $player->number,
-                    //     'position' => PositionType::from($player->position)->name,
-                    //     'rating'   => $statistics->sole(fn ($statistic) => $statistic['id'] === $player->id)['rating'],
-                    //     'img'      => $image
-                    // ];
-                    
-                    return (object)[
-                        'id'       => $player->id,
-                        'name'     => $player->name,
-                        'number'   => $player->number,
-                        'position' => PositionType::from($player->position)->name,
-                        'rating'   => $statistics->sole(fn ($statistic) => $statistic->player->id === $player->id)->statistics[0]->games->rating,
-                        'img'      => $image
-                    ];
-                });
-                
-            $players = $players->values();
-            
-            $players = collect([
-                'FW' => collect([
-                    $players[0],
-                    $players[1],
-                    $players[2],
-                ]),
-                'MID' => collect([
-                    'line' => [[
-                        $players[3],
-                        $players[4],
-                        $players[5]
-                    ]]
-                ]),
-                'DF' => collect([
-                    $players[6],
-                    $players[7],
-                    $players[8],
-                    $players[9],
-                ]),
-                'GK' => collect([
-                    $players[10]
-                ])
-            ]);
-
-            return view('players', compact('players'));
+            return view('players', $getLineup->execute($fixtureId));
 
         } catch (Exception $e) {
 
         }
-    }
-
-    public function fixtures()
-    {
-        $path = app_path('Template/fixtures');
-        
-        $fileName = $path.'/'.now()->year.'_fixtures'.'.json';
-
-        $json = File::get($fileName);
-
-        $fixtures = json_decode($json)->response;
     }
 
     public function fetchPlayerStatistics()
@@ -205,50 +106,45 @@ final class PlayerController extends Controller
         File::put($fileName, $response->getBody());
     }
 
-    public function statistic(int $id = 1035327)
+    public function fetchStartingXI(int $id = 1035327)
     {
-        $statisticsPath = app_path('Template/statistics');
+        $client = new Client();
 
-        $fileName = $statisticsPath.'/'.$id.'_player_statistic.json';
+        $response = $client->request('GET', 'https://api-football-v1.p.rapidapi.com/v3/fixtures/lineups', [
+            'query' => [
+                'fixture' => 1035327,
+                'team'    => 49
+            ],
+            'headers' => [
+                'X-RapidAPI-Host' => 'api-football-v1.p.rapidapi.com',
+                'X-RapidAPI-Key' => 'bfb866136fmshd1efe44101227e4p1a798cjsn7558902ddea2',
+            ],
+        ]);
 
-        $json = File::get($fileName);
-
-        $statistic = json_decode($json)->response;
-
-        $ChelseaStatistic = collect($statistic)->filter(function ($team) {
-            return $team->team->id === 49;
-        })->sole();
-
-        $ratings = collect($ChelseaStatistic->players)
-            ->map(function ($player) {
-                return [
-                    'id'     => $player->player->id,
-                    'name'   => $player->player->name,
-                    'rating' => $player->statistics[0]->games->rating
-                ];
-            });
+        // dd($response->getBody()->getContents());
         
-        return $ratings;
-    }
+        $path = app_path('Template/startingXI');
 
-    public function startingXI(int $id = 1035327)
-    {
-        $statisticsPath = app_path('Template/statistics');
+        if (!file_exists($path)) {
+            mkdir($path, 0777, true);
+        }
 
-        $fileName = $statisticsPath.'/'.$id.'_player_statistic.json';
+        $fileName = $path.'/'.$id.'_starting_xi.json';
 
-        $json = File::get($fileName);
+        File::put($fileName, $response->getBody()->getContents());
 
-        $statistic = json_decode($json)->response;
+        // $json = File::get($fileName);
 
-        $ChelseaStatistic = collect($statistic)->filter(function ($team) {
-            return $team->team->id === 49;
-        })->sole();
+        // $statistic = json_decode($json)->response;
 
-        $startingPlayers = collect($ChelseaStatistic->players)
-            ->filter(fn ($player) => !$player->statistics[0]->games->substitute);
+        // $ChelseaStatistic = collect($statistic)->filter(function ($team) {
+        //     return $team->team->id === 49;
+        // })->sole();
+
+        // $startingPlayers = collect($ChelseaStatistic->players)
+        //     ->filter(fn ($player) => !$player->statistics[0]->games->substitute);
         
-        return $startingPlayers;
+        // return $startingPlayers;
     }
 
     public function fetchSquads()
@@ -276,18 +172,5 @@ final class PlayerController extends Controller
         $fileName = $path.'/'.now()->year.'_post_summer.json';
         
         File::put($fileName, $response->getBody());
-    }
-
-    public function squads()
-    {
-        $path = app_path('Template/squads');
-
-        $fileName = $path.'/'.now()->year.'_post_summer.json';
-
-        $json = File::get($fileName);
-
-        $squads = json_decode($json)->response;
-
-        dd($squads);
     }
 }
--- src/app/Http/Controllers/PositionType.php
+++ src/app/Http/Controllers/PositionType.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers;
 
 enum PositionType: string
 {
-    case FW  = 'Attacker';
-    case MID = 'Midfielder';
-    case DEF = 'Defender';
-    CASE GK  = 'Goalkeeper';
+    case FW  = 'F';
+    case MID = 'M';
+    case DEF = 'D';
+    CASE GK  = 'G';
 }
