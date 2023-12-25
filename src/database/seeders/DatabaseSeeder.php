13e106bdd5caab427f046702253d54d206473fe6
watter46 <w.aoki.14@gmail.com>
2023-12-25 14:50:23 +0900

    delete: FootApiとStatisticを削除する

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             FixtureSeeder::class,
-            // LineupSeeder::class,
             // StatisticSeeder::class
         ]);
     }
