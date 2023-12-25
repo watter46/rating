d55a73671f9c24ca4d8fcbc92265fd72b08043c2
watter46 <w.aoki.14@gmail.com>
2023-12-25 20:52:49 +0900

    fix: #24_scoreの表示を修正する

--- src/resources/views/components/score/score.blade.php
+++ src/resources/views/components/score/score.blade.php
@@ -1,50 +1,50 @@
 <div class="w-full p-2 rounded-2xl bg-sky-950 h-1/3">
     <div class="bg-sky-900 rounded-2xl">
         <div class="flex items-center p-2">
-            @if ($fixture['league']['img'])
-                <img src="data:image/png;base64,<?= $fixture['league']['img'] ?>"
+            @if ($league['img'])
+                <img src="{{ $league['img'] }}"
                     class="w-16 h-16 rounded-full cursor-pointer">
             @endif
 
-            @unless($fixture['league']['img'])
+            @unless($league['img'])
                 <div class="w-16 h-16 bg-gray-400 rounded-full cursor-pointer">
                 </div>
             @endunless
 
             <div class="flex flex-col justify-center">
                 <p class="text-2xl font-black" style="text-shadow: #b8b8b8 2px 1px 5px; color: #37003C;">
-                    {{ $fixture['league']['league_name'] }}
+                    {{ $league['name'] }}
                 </p>
                 
                 <p class="font-black text-center" style="text-shadow: #b8b8b8 2px 1px 5px; color: #37003C;">
-                    {{ $fixture['league']['round'] }}
+                    {{ $league['round'] }}
                 </p>
             </div>
         </div>
     </div>
 
     <div class="flex justify-around w-full">        
-        @if ($fixture['home'])
-            <div class="flex flex-col items-center p-2">
-                @if ($fixture['home']['img'])
-                    <img src="data:image/png;base64,<?= $fixture['home']['img'] ?>"
+        @if ($teams['home'])
+            <div class="flex flex-col items-center w-1/3 p-2">
+                @if ($teams['home']['img'])
+                    <img src="{{ $teams['home']['img'] }}"
                         class="w-20 h-20 rounded-full cursor-pointer">
                 @endif
 
-                @unless($fixture['home']['img'])
+                @unless($teams['home']['img'])
                     <div class="w-20 h-20 bg-gray-400 rounded-full cursor-pointer">
                     </div>
                 @endunless
 
-                <p class="p-2 text-xl font-black text-center text-gray-300">{{ $fixture['home']['team_name'] }}</p>
+                <p class="p-2 text-xl font-black text-center text-gray-300">{{ $teams['home']['name'] }}</p>
             </div>
         @endif
 
-        <div class="pt-2 text-5xl font-black text-gray-300">
-            <div class="flex items-start text-center">
-                <p class="p-2">{{ $fixture['home']['score'] }}</p>
+        <div class="w-1/3 pt-2 text-5xl font-black text-gray-300">
+            <div class="flex items-start justify-center">
+                <p class="p-2">{{ $score['fulltime']['home'] }}</p>
                 <p class="p-2">-</p>
-                <p class="p-2">{{ $fixture['away']['score'] }}</p>
+                <p class="p-2">{{ $score['fulltime']['away'] }}</p>
             </div>
             
             <div class="text-center">
@@ -52,19 +52,19 @@
             </div>
         </div>
 
-        @if ($fixture['away'])
-            <div class="flex flex-col items-center p-2">
-                @if ($fixture['away']['img'])
-                    <img src="data:image/png;base64,<?= $fixture['away']['img'] ?>"
+        @if ($teams['away'])
+            <div class="flex flex-col items-center w-1/3 p-2">
+                @if ($teams['away']['img'])
+                    <img src="{{ $teams['away']['img'] }}"
                         class="w-20 h-20 rounded-full cursor-pointer">
                 @endif
 
-                @unless($fixture['away']['img'])
+                @unless($teams['away']['img'])
                     <div class="w-20 h-20 bg-gray-400 rounded-full cursor-pointer">
                     </div>
                 @endunless
 
-                <p class="p-2 text-xl font-black text-center text-gray-300">{{ $fixture['away']['team_name'] }}</p>
+                <p class="p-2 text-xl font-black text-center text-gray-300">{{ $teams['away']['name'] }}</p>
             </div>
         @endif
     </div>
