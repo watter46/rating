f51fac8b350292df0b0ee7dab20f3902bb492c9a
watter46 <w.aoki.14@gmail.com>
2024-02-13 15:50:18 +0900

    style: #74_Tournamentのselectorを実装する

--- src/resources/views/livewire/fixtures/fixtures.blade.php
+++ src/resources/views/livewire/fixtures/fixtures.blade.php
@@ -1,20 +1,37 @@
 <div class="flex w-screen pb-10 ">
-    <div class="w-full h-full p-2">
+    <div class="w-full h-full p-2 md:px-8">
         <div class="relative flex items-center justify-center w-full h-full">
             <div class="flex items-center w-full h-16">
                 {{ $this->fixtures->links('components.wire-pagination') }}
             </div>
 
             {{-- SortTournament --}}
-            <div class="absolute flex items-center justify-center w-2/3 px-5">
-                <select id="tournaments" class="border border-gray-300 rounded-lg block h-full w-full p-2.5 bg-gray-700 border-transparent focus:border-transparent focus:ring-0 dark:border-gray-600 dark:placeholder-gray-400 text-gray-300"
-                    wire:model.live="sort">
-                    @foreach($tournaments as $tournament)
-                        <option value="{{ $tournament['value'] }}">
-                            {{ $tournament['text'] }}
-                        </option>
-                    @endforeach
-                </select>
+            <div class="absolute flex justify-center w-[60%] z-[99]"
+                x-data="{ isOpen: false }">
+                <div class="relative flex justify-between items-center w-full bg-gray-500 rounded-lg px-2.5 py-0.5"
+                    @click="isOpen = !isOpen"
+                    @click.outside="isOpen = false">
+                    <p class="px-3 text-base text-gray-300">
+                        @foreach($tournaments as $tournament)
+                            @if($tournament['value'] === $sort)
+                                {{ $tournament['text'] }}
+                            @endif
+                        @endforeach
+                    </p>
+                    <x-svg.caret-down class="w-8 h-8 fill-gray-300" />
+
+                    <div x-show="isOpen"
+                        x-cloak
+                        class="absolute left-0 w-full py-2 mt-3 overflow-hidden bg-gray-500 rounded-lg top-full gap-y-1">
+                        @foreach($tournaments as $tournament)
+                            <p class="{{ $sort === $tournament['value'] ? 'bg-sky-700' :'' }} px-3 text-md md:text-xl text-gray-300
+                                hover:bg-sky-900"
+                                wire:click="$set('sort', '{{ $tournament['value'] }}')">
+                                {{ $tournament['text'] }}
+                            </p>
+                        @endforeach
+                    </div>
+                </div>
             </div>
         </div>
         
