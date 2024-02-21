976e6aef6d363810a56349f7647d236e2ab95811
watter46 <w.aoki.14@gmail.com>
2024-02-22 00:48:51 +0900

    fix: RateAllでレーティングが変更されるまで表示を薄くする

--- src/app/Livewire/RateAll/RateAll.php
+++ src/app/Livewire/RateAll/RateAll.php
@@ -1,36 +1,37 @@
 <?php declare(strict_types=1);
 
-namespace App\Livewire\Lineups;
+namespace App\Livewire\RateAll;
 
-use App\Livewire\MessageType;
 use Exception;
 use Illuminate\Support\Collection;
 use Livewire\Component;
-use Illuminate\Support\Str;
+use Livewire\Attributes\On;
 
+use App\Livewire\MessageType;
+use App\Livewire\RateAll\RateAllResource;
 use App\UseCases\Player\FetchPlayersUseCase;
 use App\UseCases\Player\RateAllPlayersUseCase;
-use App\Livewire\RateAllPlayersResource;
 
 
-class RateAllPlayers extends Component
+class RateAll extends Component
 {
     public array $lineups;
     public string $fixtureId;
 
+    public Collection $profiles;
     public Collection $players;
-    public bool $canRated;
 
     private readonly RateAllPlayersUseCase $rateAllPlayers;
     private readonly FetchPlayersUseCase $fetchPlayers;
-    private readonly RateAllPlayersResource $resource;
+    private readonly RateAllResource $resource;
 
     private const RATED_MESSAGE = 'Rated!!';
+    private const VALIDATE_ERROR = 'Some players are not evaluated.';
     
     public function boot(
         RateAllPlayersUseCase $rateAllPlayers,
         FetchPlayersUseCase $fetchPlayers,
-        RateAllPlayersResource $resource)
+        RateAllResource $resource)
     {
         $this->rateAllPlayers = $rateAllPlayers;
         $this->fetchPlayers = $fetchPlayers;
@@ -44,19 +45,7 @@ class RateAllPlayers extends Component
 
     public function render()
     {
-        return view('livewire.lineups.rate-all-players');
-    }
-
-    /**
-     * ラストネームに変換する
-     * @param string $name
-     * @return string
-     */
-    public function toLastName(string $name): string
-    {
-        $shortName = Str::afterLast($name, ' ');
-
-        return $shortName;
+        return view('livewire.rate-all.rate-all');
     }
     
     /**
@@ -67,18 +56,26 @@ class RateAllPlayers extends Component
     private function fetch(): void
     {
         try {
-            $playerInfoIdList = $this->resource
+            $playerInfoIds = $this->resource
                 ->lineupsToPlayers($this->lineups)
                 ->pluck('id');
                 
-            $data = $this->fetchPlayers->execute($playerInfoIdList, $this->fixtureId);
+            $data = $this->fetchPlayers->execute($playerInfoIds, $this->fixtureId);
 
-            $this->players  = $this->resource->format($this->lineups, $data->get('players'));
-            $this->canRated = $data->get('canRated');
-
+            $playersData = $this->resource->format($this->lineups, $data->get('players'));
+            
+            $this->profiles = $playersData->get('profiles');
+            $this->players  = $playersData->get('players');
+            
         } catch (Exception $e) {
             $this->dispatch('notify', message: MessageType::Error->toArray($e->getMessage()));
         }
+    }
+
+    #[On('modal-opened-rate-all')]
+    public function refetch(): void
+    {        
+        $this->fetch();
     }
     
     /**
@@ -90,10 +87,16 @@ class RateAllPlayers extends Component
     public function rateAll(array $players): void
     {
         try {
+            $validated = collect($players)->every(fn(array $player) => $player['rating']);
+            
+            if (!$validated) {
+                throw new Exception(self::VALIDATE_ERROR);
+            }
+
             $this->rateAllPlayers->execute($this->fixtureId, collect($players));
 
-            $this->dispatch('player-mom-decided');
             $this->dispatch('notify', message: MessageType::Success->toArray(self::RATED_MESSAGE));
+            $this->dispatch('close');
 
         } catch (Exception $e) {
             $this->dispatch('notify', message: MessageType::Error->toArray($e->getMessage()));
